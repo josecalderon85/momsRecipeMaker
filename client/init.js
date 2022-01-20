9c3bc5568682cc6f88dd1295415808ebfdefe080
Jose Calderon <40038775+josecalderon85@users.noreply.github.com>
2022-01-20 12:46:49 -0600

    Built forms and auth sample

--- client/init.js
+++ client/init.js
@@ -1,1 +1,64 @@
-console.log("running the init")
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 0,
+    };
+    this.pageFlip = this.pageFlip.bind(this);
+    this.pageRender = this.pageRender.bind(this);
+  }
+  pageFlip() {
+    console.log(this.props);
+  }
+  pageRender() {
+    switch (this.state.page) {
+      case 0:
+        return <KeyForm />;
+      case 1:
+        return <BasicInfoForm />;
+      default:
+        break;
+    }
+  }
+  render() {
+    return <div className="App">{this.pageRender()}</div>;
+  }
+}
+
+var KeyForm = function () {
+  return (
+    <div>
+      <form action="/validate" method="GET">
+        <label for="key">Llave:</label>
+        <br />
+        <input type="text" id="key" name="key" />
+        <br />
+        <br />
+        <input type="submit" value="Entra" />
+      </form>
+    </div>
+  );
+};
+var BasicInfoForm = function () {
+  return (
+    <div>
+      <form action="/basicInfo" method="POST">
+        <label for="name">Nombre:</label>
+        <br />
+        <input type="text" id="name" name="name" />
+        <br />
+        <label for="email">Email:</label>
+        <br />
+        <input type="text" id="email" name="email" />
+        <br />
+        <label for="phone">Telefono:</label>
+        <br />
+        <input type="text" id="phone" name="phone" />
+        <br />
+        <br />
+        <input type="submit" value="Entra" />
+      </form>
+    </div>
+  );
+};
+ReactDOM.render(<App />, document.getElementById("app"));
--- server/server.js
+++ server/server.js
@@ -4,10 +4,25 @@ const port = 3000
 const path = require('path')
 
 app.use('/', express.static(path.join(__dirname, '../client')))
+app.use(express.json());
+app.use(express.urlencoded())
 
-app.get('/server', (req, res) => {
-  res.send('Hello World!')
+var validKeys = [
+    "9xxq9I8ke8LyIVKE",
+    "rd2pVlK70cd1rqBO",
+    "7gdXx2LJbKKlwy1y"
+]
+
+
+app.get('/validate', (req, res) => {
+    console.log("this is what we got from the client",req.query)
+    if(validKeys.includes(req.query.key)){
+        res.send('Step 2')
+    }else{
+        res.send('No eres digno, Alejate!')
+    }
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
